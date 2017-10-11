ae1b8d4682ee3d5c3f5d191b54ede83c6512c12f
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2017-10-11 02:12:38 +0000

    add unless to wp::command so we can stop always activating theme

--- manifests/command.pp
+++ manifests/command.pp
@@ -1,14 +1,16 @@
 define wp::command (
 	$location,
-	$command
+	$command,
+  $unless = undef
 ) {
 	include wp::cli
 
 	exec {"$location wp $command":
-		command => "/usr/bin/wp $command",
-		cwd => $location,
-		user => $::wp::user,
-		require => [ Class['wp::cli'] ],
-		onlyif => '/usr/bin/wp core is-installed'
+    command => "/usr/bin/wp $command",
+    cwd     => $location,
+    user    => $::wp::user,
+    require => [ Class['wp::cli'] ],
+    onlyif  => '/usr/bin/wp core is-installed',
+    unless  => $unless
 	}
 }
--- manifests/theme.pp
+++ manifests/theme.pp
@@ -8,13 +8,16 @@ define wp::theme (
 	case $ensure {
 		enabled: {
 			$command = "activate $title"
+      $unless = "/usr/bin/test `${wp::params::bin_path}/wp theme list --name=${title} --field=status $slug` = active"
+
 		}
 		default: {
 			fail("Invalid ensure for wp::theme")
 		}
 	}
 	wp::command { "$location theme $command":
-		location => $location,
-		command => "theme $command"
+    location => $location,
+    command  => "theme $command",
+    unless   => $unless
 	}
 }
